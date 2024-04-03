73a9a3656b943f3462d73c7d8abf486dd56c097c
Titanantony <antonytinan96@gmail.com>
2024-04-03 17:08:28 +0300

    added firebase to the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flashscore_clone/firebase_options.dart';
 import 'package:flashscore_clone/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
